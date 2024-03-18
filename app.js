c0ebb8c5dccb01401ccb0f30f7a0a5e51cf564ba
Ann Maria Jose <annmariajose1412@gmail.com>
2024-03-17 23:40:54 -0700

    Create a database, collection and insert a document

---
+++ app.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require('mongodb')
+
+// Create instance of MongoClient for mongodb
+const client = new MongoClient('mongodb://127.0.0.1:27017/nodemongo')
+
+// Connect to database
+client.connect()
+    .then(() => {
+        //console.log('Connected Succesfully & Database Created!')
+        // Close the database connection
+        //client.close();
+
+        var dbo = client.db('nodemongo');
+        /* dbo.createCollection('customers').then(function() {
+            console.log("Collection created");
+            // Close the database connection
+            client.close();
+        }) */
+
+        var custData = { name: "Westcliff", address: "Irvine, CA" };
+        dbo.collection("customers").insertOne(custData).then(function() {
+            console.log("1 document inserted")
+            client.close()
+        })
+    })
+    .catch(error => console.log('Failed to connect', error))
